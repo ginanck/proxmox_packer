5d9e41a0473e25cb8feaa68d569902deb5336ac1
Gorkem Korkmaz <gorkeminanckorkmaz@gmail.com>
2025-03-06 00:11:42 +0300

    grub configuration issue has been fixed

--- scripts/ubuntu-2204/04-grub.sh
+++ scripts/ubuntu-2204/04-grub.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# NOTE: boot_command issue
+# NOTE: boot_command issue
 # https://github.com/hashicorp/packer-plugin-vsphere/issues/421
+DISTRIB=$( lsb_release -i -s 2> /dev/null || echo Debian )
 
 sed -i 's/^GRUB_DEFAULT=.*$/GRUB_DEFAULT="0"/' /etc/default/grub
 sed -i 's/^GRUB_TIMEOUT_STYLE=.*$/GRUB_TIMEOUT_STYLE="hidden"/' /etc/default/grub
 sed -i 's/^GRUB_TIMEOUT=.*$/GRUB_TIMEOUT="2"/' /etc/default/grub
-sed -i 's/^GRUB_DISTRIBUTOR=.*$/GRUB_DISTRIBUTOR="`lsb_release -i -s 2> /dev/null || echo Debian`"/' /etc/default/grub
+sed -i "s/^GRUB_DISTRIBUTOR=.*$/GRUB_DISTRIBUTOR=\"$DISTRIB\"/" /etc/default/grub
 sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*$/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
 sed -i 's/^GRUB_CMDLINE_LINUX=.*$/GRUB_CMDLINE_LINUX=""/' /etc/default/grub
 update-grub
